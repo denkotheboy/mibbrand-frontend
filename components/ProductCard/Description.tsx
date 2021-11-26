8c1a2f52a520086b8784f5038cf893fb7db66b8b
Denis <kondratev_dv@sima-land.ru>
2021-11-26 18:57:57 +0500

    init

--- components/ProductCard/Description.tsx
+++ components/ProductCard/Description.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
 
 interface IProps {
   description: string;
@@ -10,12 +11,13 @@ const Description: FC<IProps> = ({ description }) => {
   return (
     <Grid item xs={12} padding={1} textAlign="start">
       <Typography
+        component="span"
         color="green"
         sx={{ fontSize: { xs: "5vw", sm: "3.5vw", md: "1.6vw" } }}
       >
         Описание
       </Typography>
-      <hr />
+      <Divider variant="middle" sx={{ margin: 2 }} />
       <Typography
         sx={{
           fontSize: 15,
@@ -26,7 +28,6 @@ const Description: FC<IProps> = ({ description }) => {
       >
         {description}
       </Typography>
-      <hr />
     </Grid>
   );
 };
--- components/ProductCard/Info.tsx
+++ components/ProductCard/Info.tsx
@@ -4,6 +4,10 @@ import Button from "@mui/material/Button";
 import { ICategory, IProduct } from "../../pages/product/[id]";
 import Link from "next/link";
 import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Chip from "@mui/material/Chip";
+import ColorLensIcon from "@mui/icons-material/ColorLens";
 
 interface IProps {
   product: IProduct;
@@ -14,45 +18,56 @@ const Info: FC<IProps> = ({ product }) => {
     <Grid item xs={12} textAlign="start" padding={1}>
       Цена:
       <Typography
+        component="span"
         color="green"
         sx={{ fontSize: { xs: "5vw", sm: "3.5vw", md: "1.6vw" } }}
       >
         {product.price} руб.
       </Typography>
-      <hr />
-      <Button variant="contained" color="success" size="large">
+      <Divider variant="middle" sx={{ margin: 2 }} />
+      <Button
+        variant="contained"
+        color="success"
+        size="large"
+        startIcon={<ShoppingCartIcon />}
+      >
         Купить сейчас
       </Button>
-      <hr />
+      <Divider variant="middle" sx={{ margin: 2 }} />
       {product.categories.length > 0 ? (
         <>
           Категория:{" "}
-          <Typography color="green">
-            {product.categories.map((categories: ICategory, i) => (
-              <Link href={`/category/${categories.type}`} passHref key={i}>
-                {categories.name}
-              </Link>
-            ))}
-          </Typography>
+          {product.categories.map((categories: ICategory, i) => (
+            <Link href={`/category/${categories.type}`} passHref key={i}>
+              <Chip
+                label={categories.name}
+                variant="outlined"
+                color="success"
+                size="small"
+              />
+            </Link>
+          ))}
+          <Divider variant="middle" sx={{ margin: 2 }} />
         </>
       ) : null}
       {product.color ? (
         <>
-          Цвет: <Typography color="green">{product.color}</Typography>
+          Цвет: <span style={{ color: "green" }}>{product.color}</span>
+          <Divider variant="middle" sx={{ margin: 2 }} />
         </>
       ) : null}
       {product.composition ? (
         <>
-          Состав: <Typography color="green">{product.composition}</Typography>
+          Состав: <span style={{ color: "green" }}>{product.composition}</span>
+          <Divider variant="middle" sx={{ margin: 2 }} />
         </>
       ) : null}
       {product.size ? (
         <>
-          Размеры:
-          <Typography color="green">{product.size}</Typography>{" "}
+          Размеры: <span style={{ color: "green" }}>{product.size}</span>
+          <Divider variant="middle" sx={{ margin: 2 }} />
         </>
       ) : null}
-      <hr />
     </Grid>
   );
 };
